198ccf7d32a0a741c6d7a94ffe217258b9c6b9fb
Nicola Di Marco <ndimarco@amp.energy>
2020-12-18 16:47:45 +0100

    featured logos margins mobile

--- src/components/FeaturedIn.svelte
+++ src/components/FeaturedIn.svelte
@@ -4,7 +4,7 @@
 
 <div class="content flex flex-col w-100pc justify-between spl text-center md:w-80pc">
   <div class="text-white bg-black rounded-4px py-6px px-8px mb-10px">FEATURED IN</div>
-    <div class="my-20px">
+    <div class="my-20px mx-20px md:mx-0 lg:mx-0">
         <img class="w-100pc h-auto" src={images.press} alt="PieDAO Featured In" />
     </div>
 </div>
